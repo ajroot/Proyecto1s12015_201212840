0377603f3690401e25cfac7083e29e8f3d3856ab
Javier <alexjavierfuentes@hotmail.com>
2015-03-15 18:13:37 -0600

    estructuras
    modificaciones al arbol

--- estructuras_python/Nodo.py
+++ estructuras_python/Nodo.py
@@ -1,2 +1,11 @@
 class Nodo:
-"""
+"""Clase nodo de arbol"""
+
+def _init_(self,key):
+	"""Constructor _init_ self llamada a si mismo"""
+	self.izquierdo=None
+	self.derecho=None
+	self.key=key
+	
+def _str_(self):
+	return "%s" % self.key
--- estructuras_python/arbolAVL.py
+++ estructuras_python/arbolAVL.py
@@ -1,4 +1,34 @@
 class arbolAVL():
 
 def _init_(self):"""_init_ es para inicializar (constructor)"""
+		self.nodo=None
+		self.altura=-1
+		self.balance=0
+
+def insertar(self,key):
+	"""Insertar"""
+	n=Nodo(key)
+	
+	"""para el primer arbolAVL"""
+	if self.nodo== None:
+		self.nodo=n
+		self.nodo.izquierdo=arbolAVL()
+		self.nodo.derecho=arbolAVL()
+		"""Insertar llave en subarbol izquierdo"""
+	elif key < self.nodo.key:
+		self.nodo.izquierdo.insertar(key)
+	"""insertar llave en subarbol derecho"""
+	elif key < self.nodo.key:
+		self.nodo.derecho.insertar(key)
 		
+		"""la llave fue insertada y existe en el arbol"""
+		
+		"""Rebalancear"""
+		self.rebalance()
+		
+
+def rebalance(self):
+"""rebalancear el arbol siempre despues de eliminar, insertar
+o cualquier modificacion al arbol"""
+	
+
